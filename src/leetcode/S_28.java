5ca7726b838bff542f362b9fa76ab5e91bb55eda
nilknow <maxwangein@gmail.com>
2024-05-27 20:54:20 +0800

    leetcode 28 solution KMP

--- src/leetcode/S_28.java
+++ src/leetcode/S_28.java
@@ -39,8 +39,52 @@ public class S_28 {
         }
     }
 
+    static class Solution_KMP implements Solution {
+
+        @Override
+        public int strStr(String haystack, String needle) {
+            int[] next = new int[needle.length()];
+            next[0] = 0;
+            int prefix = 0;
+            int i = 1;
+            while (i < needle.length()) {
+                if (needle.charAt(prefix) == needle.charAt(i)) {
+                    prefix++;
+                    next[i] = prefix;
+                    i++;
+                } else {
+                    if (prefix == 0) {
+                        next[i] = 0;
+                        i++;
+                    } else {
+                        prefix = next[prefix - 1];
+                    }
+                }
+            }
+
+            i = 0;
+            int j = 0;
+            while (i < haystack.length()) {
+                if (haystack.charAt(i) == needle.charAt(j)) {
+                    i++;
+                    j++;
+                } else if (j > 0) {
+                    j = next[j - 1];
+                } else {
+                    i += 1;
+                }
+
+                if (j == needle.length()) {
+                    return i - j;
+                }
+            }
+
+            return -1;
+        }
+    }
+
     public static void main(String[] args) {
-        Solution solution = new Solution2();
+        Solution solution = new Solution_KMP();
         int i1 = solution.strStr("sadbutsad", "sad");
         Assert.eq(i1, 0);
         int i2 = solution.strStr("hello", "helo");
