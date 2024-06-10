8982030819408240c25574f50c34e6ab10cb1a29
nilknow <maxwangein@gmail.com>
2024-06-10 10:57:29 +0800

    leetcode 55 better solution

--- src/leetcode/S_55.java
+++ src/leetcode/S_55.java
@@ -5,26 +5,18 @@ import util.Assert;
 public class S_55 {
     static class Solution {
         public boolean canJump(int[] nums) {
-            if(nums.length==1){
-                return true;
-            }
-            if(nums[0]==0){
-                return false;
-            }
-            int maxDistance=nums[0];
-            for(int i=1;i<nums.length;i++){
-                if(i>maxDistance){
-                    return false;
-                }
-                int num=nums[i];
-                if(i+num>maxDistance){
-                    maxDistance=i+num;
-                    if(maxDistance>=nums.length){
+            int rightmost=0;
+            for(int i=0;i<nums.length;i++){
+                if(i<=rightmost){
+                    rightmost=Math.max(rightmost,i+nums[i]);
+                    if(rightmost>=nums.length-1){
                         return true;
                     }
+                }else{
+                    return false;
                 }
             }
-            return true;
+            return false;
         }
     }
 
