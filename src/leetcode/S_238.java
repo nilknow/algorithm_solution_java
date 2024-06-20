66254660ad5947a1a6b2842db131d58f9925a929
nilknow <maxwangein@gmail.com>
2024-06-20 11:03:32 +0800

    leetcode 238

---
+++ src/leetcode/S_238.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class S_238 {
+    static class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int[] ans = new int[nums.length];
+            ans[0] = 1;
+            for (int i = 1; i < nums.length; i++) {
+                ans[i] = ans[i - 1] * nums[i - 1];
+            }
+            int r = 1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                ans[i] = ans[i] * r;
+                r *= nums[i];
+            }
+            return ans;
+        }
+    }
+}
