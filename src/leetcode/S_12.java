c262a24204cefd79f0f797ca9eb823481179ef61
nilknow <maxwangein@gmail.com>
2024-07-22 11:29:12 +0800

    leetcode 12

---
+++ src/leetcode/S_12.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class S_12 {
+    private static class Solution {
+        public String intToRoman(int num) {
+            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] rom = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < values.length; i++) {
+                while (num >= values[i]) {
+                    sb.append(rom[i]);
+                    num -= values[i];
+                }
+            }
+
+            return sb.toString();
+        }
+    }
+}
